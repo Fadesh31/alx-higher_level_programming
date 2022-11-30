7c3419e5292c8e2afbb55c88e5472a0d6e04042a
Fadesola Okunade <68782497+Fadesh31@users.noreply.github.com>
2022-11-30 07:52:15 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(26):
-    if char != 4 and char != 16:
-        print("{:s}".format(chr(char + ord("a"))), end="")
+for letter in range(ord('a'), ord('z') + 1):
+    if letter != 113 and letter != 101:
+        print('{}'.format(chr(letter)), end='')
